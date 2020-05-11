1d14a699446f3e30220bf6559818fdcdf0fc5136
Carlos Rodríguez <carlosguillermoru@gmail.com>
2020-05-11 17:10:57 +0200

    Implements Getters, setters and toString

--- src/main/java/bingo/Bingo.java
+++ src/main/java/bingo/Bingo.java
@@ -24,12 +24,43 @@ public abstract class Bingo {
     }
     
     public Bingo(String name){
-        //Only insert player identification
+        //Only insert player name
         Random r = new Random();
         this.name = name;
         this.fecha = LocalDate.now();
         this.id = Integer.toString(r.nextInt(999999) + (char)(r.nextInt(93) + 65));
     }
+     // Getters and setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return ("el jugador " + this.name + " en la partida con ID " + this.id + " el día " + this.fecha);
+    }
+    
     
     
 }
