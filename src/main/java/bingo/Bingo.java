0f1a497b51770f94b275b3034003398386cc9647
Carlos Rodríguez <carlosguillermoru@gmail.com>
2020-05-11 17:09:00 +0200

    Implements constructors

--- src/main/java/bingo/Bingo.java
+++ src/main/java/bingo/Bingo.java
@@ -5,10 +5,31 @@
  */
 package bingo;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 /**
  *
  * @author carlos
  */
 public abstract class Bingo {
+    private String id;
+    private LocalDate fecha;
+    private String name;
+
+    public Bingo(String id, LocalDate fecha, String name) {
+        this.id = id;
+        this.fecha = fecha;
+        this.name = name;
+    }
+    
+    public Bingo(String name){
+        //Only insert player identification
+        Random r = new Random();
+        this.name = name;
+        this.fecha = LocalDate.now();
+        this.id = Integer.toString(r.nextInt(999999) + (char)(r.nextInt(93) + 65));
+    }
+    
     
 }
