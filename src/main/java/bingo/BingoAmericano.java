0d3bb4c7fb03ff79bc687cc78e0241f6f721e7cd
Carlos Rodríguez <carlosguillermoru@gmail.com>
2020-05-12 17:58:13 +0200

    Implements constructor and toString

--- src/main/java/bingo/BingoAmericano.java
+++ src/main/java/bingo/BingoAmericano.java
@@ -10,5 +10,17 @@ package bingo;
  * @author carlos
  */
 public final class BingoAmericano {
+    private CartonAmericano carton;
+    private BomboAmericano bombo;
+
+    public BingoAmericano(CartonAmericano carton, BomboAmericano bombo) {
+        this.carton = carton;
+        this.bombo = bombo;
+    }
+
+    @Override
+    public String toString() {
+        return "BingoAmericano{" + "carton=" + carton + ", bombo=" + bombo + '}';
+    }
     
 }
