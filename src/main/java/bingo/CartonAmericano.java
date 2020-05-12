efe62b85de3b8439334e149e8c3b94d0ee32be03
Carlos Rodríguez <carlosguillermoru@gmail.com>
2020-05-12 17:55:28 +0200

    Implements constructor

--- src/main/java/bingo/CartonAmericano.java
+++ src/main/java/bingo/CartonAmericano.java
@@ -10,5 +10,16 @@ package bingo;
  * @author carlos
  */
 public final class CartonAmericano extends Carton {
+    public static final int FILAS = 5;
+    public static final int COLUMNAS = 5;
+
+    public CartonAmericano(int filas, int columnas) {
+        super(filas, columnas);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
     
 }
