d1f07b6a31665032ba109942f30566231b6223a3
Carlos Rodríguez <carlosguillermoru@gmail.com>
2020-05-18 21:31:52 +0200

    Implements some patterns

--- src/main/java/bingo/Patron.java
+++ src/main/java/bingo/Patron.java
@@ -5,10 +5,66 @@
  */
 package bingo;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author carlos
  */
 public enum Patron {
-    
+    FORMA_X(equis(), "X"),
+    FORMA_F(efe(), "F");
+    private List<Point> casillas;
+    private String descripcion;
+
+    private Patron(List<Point> casillas, String descripcion) {
+        this.casillas = casillas;
+        this.descripcion = descripcion;
+    }
+
+    public List<Point> getCasillas() {
+        return casillas;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+    private static ArrayList<Point> equis(){
+        ArrayList<Point> tmp = new ArrayList<>();
+        tmp.add(new Point(0, 0));
+        tmp.add(new Point(1,1));
+        tmp.add(new Point(2,2));
+        
+        tmp.add(new Point(3, 1));
+        tmp.add(new Point(4, 0));
+        
+        tmp.add(new Point(0, 4));
+        tmp.add(new Point(1, 3));
+        tmp.add(new Point(3,3));
+        tmp.add(new Point(4,4));
+        return tmp;
+    }
+    private static ArrayList<Point> efe(){
+        ArrayList<Point> tmp = new ArrayList<>();
+        
+        tmp.add(new Point(0,0));
+        tmp.add(new Point(0,1));
+        tmp.add(new Point(0,2));
+        tmp.add(new Point(0,3));
+        tmp.add(new Point(0,4));
+        
+        tmp.add(new Point(1,0));
+        
+        tmp.add(new Point(2,0));
+        tmp.add(new Point(2,1));
+        tmp.add(new Point(2,2));
+        
+        tmp.add(new Point(3,0));
+        
+        tmp.add(new Point(4,0));
+        
+        return tmp;
+    }
 }
