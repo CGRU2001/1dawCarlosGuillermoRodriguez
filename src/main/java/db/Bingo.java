c5fb19794ed53f34c18dcfc99eec959fd0977c78
Carlos Rodríguez <carlosguillermoru@gmail.com>
2020-05-28 11:48:42 +0200

    Crea la clase bingo

---
+++ src/main/java/db/Bingo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import bingo.*;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author carlos
+ */
+public interface Bingo {
+
+    Bingo findByPk(int pk) throws SQLException;
+
+    int insertGame(Bingo b) throws SQLException;
+
+    int deleteGame (Bingo b) throws SQLException;
+    
+    boolean updateGame(int pk, Bingo b) throws SQLException;
+}
