d50e8e0ae58dbbc46090d23fe9967970f2f708bb
Carlos Rodríguez <carlosguillermoru@gmail.com>
2020-05-29 12:36:25 +0200

    Implements dump and retore

--- src/main/java/bingo/JuegoBingo.java
+++ src/main/java/bingo/JuegoBingo.java
@@ -3,17 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package bingo;
 
 import db.*;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author carlos
  */
 public class JuegoBingo {
+
     public static void main(String[] args) {
         //Place main content here
-        Conexion.getInstance();
+
+        BingoDAO bd = new BingoDAO();
+
+        BingoAmericano ba = new BingoAmericano(new CartonAmericano(), new BomboAmericano(), "");
+        
+        
+        
+        
+
+    }
+    
+    public static void dump(BingoAmericano ba){
+        BingoDAO bd = new BingoDAO();
+        try {
+            bd.insertGame(ba);
+        } catch (SQLException ex) {
+            Logger.getLogger(JuegoBingo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public static BingoAmericano restore(BingoAmericano ba, String serial){
+        BingoDAO bd = new BingoDAO();
+        try {
+            ba = bd.findByPk(serial);
+            System.out.println("----- RESTAURAR PARTIDA -----");
+            System.out.println("Bienvenido de nuevo, " + ba.getName());
+            System.out.println(ba.getCarton());
+            return ba;
+        } catch (Exception ex) {
+            System.out.println("Error: No se pudo aplicar el recurso. Error " + ex.getMessage() + ". Se generará una partida nueva");
+            return new BingoAmericano(new CartonAmericano(), new BomboAmericano(), "");
+        }
     }
 }
